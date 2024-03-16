fcc16471955d85b5856e124838d02a4c242e420e
Jaroslav Sykora <jaroslav.sykora@gmail.com>
2024-03-16 23:36:57 +0100

    fix position of forth code: it should be in cpu bank 8 to coincide with ROMBLOCK=3

--- platforms/X65/load.sh
+++ platforms/X65/load.sh
@@ -3,7 +3,9 @@
 # set variable X65PYHOST to the path of the x65pyhost directory
 X65PYHOST=$HOME/ownCloud/elektronika/Open65/x65pyhost
 
-# execute do-loadbin.py to load the forth binary to the bank $08 of the connected hw
+echo "Reset the X65-SBC in ROMBLOCK=3 with bits 6 and 7 set to 1"
+$X65PYHOST/do-cpureset.py -r 0xc3
+
+echo "Load the forth binary to the '816 CPU bank 0x08, which coincides with ROMBLOCK=3"
 $X65PYHOST/do-loadbin.py of816-x65.bin sram 0x080000
 
-echo "Now reset the X65-SBC in ROMBLOCK=3"
